ecfbb369c9defd4ea42a76c7349df62ddd898959
clmntgbr <clement.goubier@gmail.com>
2022-02-01 11:53:06 +0100

    Adding admin feature

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('@EasyAdmin/page/content.html.twig');
     }
 
     public function configureDashboard(): Dashboard
